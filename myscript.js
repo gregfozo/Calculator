d9dbbade995b8899a961106849696b5941a5949d
Greg Fozo <gregcook2011@gmail.com>
2017-07-18 12:15:28 +0200

    Add jscript

---
+++ myscript.js
@@ -0,0 +1,18 @@
+//Get all the keys from document
+var key = document.querySelectorAll();
+var operators = ['+', '-'];
+var decimalAdded = false;
+
+//Add onclick event to all the keys and perform operations
+for (var i = 0; i < key.length; i++) {
+    keys[i].onclick = function(e) {
+        //Get the input and button values
+        var input = document.querySelector('.input');
+        var inputVal = input.innerHTML;
+        var btn Val = this.innerHTML;
+        if (btnVal = = '=') {
+            var equation = inputVal;
+            var last Char = equation[equation.length - 1];
+        }
+    }
+}
